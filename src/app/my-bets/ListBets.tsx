87347475394c18d52114821435c618f0528d5fe4
lucas vinicius alencar alves <lucasviniciusaalves@gmail.com>
2023-04-07 23:23:41 -0300

    update: List tickets

--- src/app/my-bets/ListBets.tsx
+++ src/app/my-bets/ListBets.tsx
@@ -5,6 +5,11 @@ import {
   AccordionItem,
   AccordionPanel,
 } from '@chakra-ui/react'
+import {
+  RiCheckboxBlankCircleFill,
+  RiCheckboxCircleFill,
+  RiCloseCircleFill,
+} from 'react-icons/ri'
 
 type Props = {
   bets: IBet[]
@@ -42,17 +47,35 @@ export function ListBets({ bets }: Props) {
             <AccordionPanel px={0} pb={4}>
               <hr className="border-[#5d5d5d]" />
               {bet.matches.map((match) => (
-                <div key={match.id} className="px-4 mt-3">
-                  <div className="flex justify-between">
-                    <p className="font-bold">{match.market.option}</p>
-                    <p>{match.market.odd}</p>
-                  </div>
+                <div key={match.id} className="flex px-4 mt-3">
+                  {match.status === 'won' ? (
+                    <RiCheckboxCircleFill
+                      className="text-green-400 mt-1 mr-1"
+                      size={14}
+                    />
+                  ) : match.status === 'lost' ? (
+                    <RiCloseCircleFill
+                      className="text-red-400 mt-1 mr-1"
+                      size={14}
+                    />
+                  ) : (
+                    <RiCheckboxBlankCircleFill
+                      className="text-gray-500 mt-1 mr-1"
+                      size={14}
+                    />
+                  )}
+                  <div>
+                    <div className="flex justify-between">
+                      <p className="font-bold">{match.market.option}</p>
+                      <p>{match.market.odd}</p>
+                    </div>
 
-                  <p className="text-sm">{match.market.name}</p>
-                  <p className="text-sm">
-                    {match.localTeam} x {match.visitorTeam} - {match.date}{' '}
-                    {match.time}
-                  </p>
+                    <p className="text-sm">{match.market.name}</p>
+                    <p className="text-sm">
+                      {match.localTeam} x {match.visitorTeam} - {match.date}{' '}
+                      {match.time}
+                    </p>
+                  </div>
                 </div>
               ))}
               <hr className="border-[#5d5d5d] my-3" />
