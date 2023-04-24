f4f7f7de08ffbe049a12a1736cd10f5aced1f927
lucas vinicius alencar alves <lucasviniciusaalves@gmail.com>
2023-04-24 11:43:47 -0300

    add: Sport list

--- src/app/Home/SportsList/index.tsx
+++ src/app/Home/SportsList/index.tsx
@@ -5,82 +5,99 @@ import sport2 from '@/assets/images/sports/2.svg'
 import sport3 from '@/assets/images/sports/3.svg'
 import sport5 from '@/assets/images/sports/5.svg'
 import sport6 from '@/assets/images/sports/6.svg'
+import sport10 from '@/assets/images/sports/10.svg'
+import sport16 from '@/assets/images/sports/16.svg'
 import sport20 from '@/assets/images/sports/20.svg'
-// import sport40 from '@/assets/images/sports/40.svg'
+import sport23 from '@/assets/images/sports/23.svg'
+import sport29 from '@/assets/images/sports/29.svg'
 import sport117 from '@/assets/images/sports/117.svg'
-// import eSports from '@/assets/images/sports/eSports.svg'
+
 import { Link } from 'react-router-dom'
 
 export function SportsList() {
   return (
-    <div className="flex m-0 items-center py-2 px-1 space-x-4 overflow-x-scroll scrollbar-none bg-[#5a5a5a]">
-      <Link
-        to={`/sports/${'soccer'}`}
-        className="text-center justify-center text-[#fff] w-20"
-      >
-        <Image
-          src={sport1}
-          className="block m-auto h-[32px] w-[32px]"
-          alt="sport1"
-        />
-        <p className="text-sm">Futebol</p>
+    <div className="flex m-0 items-center px-1 overflow-x-scroll scrollbar-none bg-[#5a5a5a]">
+      <Link to={`/sports/${'soccer'}`}>
+        <div className="w-20 text-center justify-center text-[#fff]">
+          <Image
+            src={sport1}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport1"
+          />
+          <p className="text-sm">Futebol</p>
+        </div>
       </Link>
-      <Link
-        to={`/sports/${'basketball'}`}
-        className="text-center justify-center text-[#fff]"
-      >
-        <Image
-          src={sport2}
-          className="block m-auto h-[32px] w-[32px]"
-          alt="sport2"
-        />
-        <p className="text-sm">Basquete</p>
+      <Link to={`/sports/${'basketball'}`}>
+        <div className="w-20 text-center justify-center text-[#fff]">
+          <Image
+            src={sport2}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport2"
+          />
+          <p className="text-sm">Basquete</p>
+        </div>
       </Link>
-      <Link
-        to={`/sports/${'tennis'}`}
-        className="text-center justify-center text-[#fff]"
-      >
-        <Image
-          src={sport5}
-          className="block m-auto h-[32px] w-[32px]"
-          alt="sport5"
-        />
-        <p className="text-sm">Tênis</p>
+      <Link to={`/sports/${'tennis'}`}>
+        <div className="w-20 text-center justify-center text-[#fff]">
+          <Image
+            src={sport5}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport5"
+          />
+          <p className="text-sm">Tênis</p>
+        </div>
       </Link>
-      <Link
-        to={`/sports/${'baseball'}`}
-        className="text-center justify-center text-[#fff]"
-      >
-        <Image
-          src={sport3}
-          className="block m-auto h-[32px] w-[32px]"
-          alt="sport3"
-        />
-        <p className="text-sm">Basebol</p>
+      <Link to={`/sports/${'table-tennis'}`}>
+        <div className="text-center justify-center text-[#fff] w-20">
+          <Image
+            src={sport20}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport20"
+          />
+          <p className="text-sm whitespace-nowrap">Tênis de Mesa</p>
+        </div>
       </Link>
-      <Link
-        to={`/sports/${'handball'}`}
-        className="text-center justify-center text-[#fff]"
-      >
-        <Image
-          src={sport6}
-          className="block m-auto h-[32px] w-[32px]"
-          alt="sport6"
-        />
-        <p className="text-sm">Handebol</p>
+      <Link to={`/sports/${'volleyball'}`}>
+        <div className="text-center justify-center text-[#fff] w-20">
+          <Image
+            src={sport23}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport6"
+          />
+          <p className="text-sm">Vôlei</p>
+        </div>
       </Link>
-      <Link
-        to={`/sports/${'table-tennis'}`}
-        className="text-center justify-center text-[#fff]"
-      >
-        <Image
-          src={sport20}
-          className="block m-auto h-[32px] w-[32px]"
-          alt="sport20"
-        />
-        <p className="whitespace-nowrap">Tênis de Mesa</p>
+      <Link to={`/sports/${'handball'}`}>
+        <div className="text-center justify-center text-[#fff] w-20">
+          <Image
+            src={sport6}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport6"
+          />
+          <p className="text-sm">Handebol</p>
+        </div>
       </Link>
-      {/* <div className="text-center justify-center text-[#fff]">
+      <Link to={`/sports/${'baseball'}`}>
+        <div className="text-center justify-center text-[#fff] w-20">
+          <Image
+            src={sport3}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport3"
+          />
+          <p className="text-sm">Basebol</p>
+        </div>
+      </Link>
+      <Link to={`/sports/${'american-football'}`}>
+        <div className="text-center justify-center text-[#fff] w-20">
+          <Image
+            src={sport16}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport16"
+          />
+          <p className="text-sm">Futebol Americano</p>
+        </div>
+      </Link>
+      {/* <div >
         <Image
           src={sport40}
           className="block m-auto h-[32px] w-[32px]"
@@ -88,18 +105,27 @@ export function SportsList() {
         />
         <p className="whitespace-nowrap">Formula 1</p>
       </div> */}
-      <Link
-        to={`/sports/${'mma'}`}
-        className="text-center justify-center text-[#fff]"
-      >
-        <Image
-          src={sport117}
-          className="block m-auto h-[32px] w-[32px]"
-          alt="sport117"
-        />
-        <p className="text-sm">MMA</p>
+      <Link to={`/sports/${'mma'}`}>
+        <div className="text-center justify-center text-[#fff] w-20">
+          <Image
+            src={sport117}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport117"
+          />
+          <p className="text-sm">MMA</p>
+        </div>
       </Link>
-      {/* <div className="text-center justify-center text-[#fff]">
+      <Link to={`/sports/${'boxe'}`}>
+        <div className="text-center justify-center text-[#fff] w-20">
+          <Image
+            src={sport10}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport10"
+          />
+          <p className="text-sm">Boxe</p>
+        </div>
+      </Link>
+      {/* <div >
         <Image
           src={eSports}
           className="block m-auto h-[32px] w-[32px]"
@@ -107,6 +133,16 @@ export function SportsList() {
         />
         <p className="whitespace-nowrap">E-Sports</p>
       </div> */}
+      <Link to={`/sports/${'futsal'}`}>
+        <div className="text-center justify-center text-[#fff] w-20">
+          <Image
+            src={sport29}
+            className="block m-auto h-[32px] w-[32px]"
+            alt="sport29"
+          />
+          <p className="text-sm">Futsal</p>
+        </div>
+      </Link>
     </div>
   )
 }
--- src/components/Siderbar/SidebarNav.tsx
+++ src/components/Siderbar/SidebarNav.tsx
@@ -1,14 +1,16 @@
 import { Stack } from '@chakra-ui/react'
 
-import sport1 from '../../assets/images/sports/1.svg'
-import sport2 from '../../assets/images/sports/2.svg'
-import sport3 from '../../assets/images/sports/3.svg'
+import sport1 from '@/assets/images/sports/1.svg'
+import sport2 from '@/assets/images/sports/2.svg'
+import sport3 from '@/assets/images/sports/3.svg'
 import sport5 from '@/assets/images/sports/5.svg'
-import sport6 from '../../assets/images/sports/6.svg'
-import sport20 from '../../assets/images/sports/20.svg'
-import sport23 from '../../assets/images/sports/23.svg'
-// import sport40 from '../../assets/images/sports/40.svg'
-import sport117 from '../../assets/images/sports/117.svg'
+import sport6 from '@/assets/images/sports/6.svg'
+import sport10 from '@/assets/images/sports/10.svg'
+import sport16 from '@/assets/images/sports/16.svg'
+import sport20 from '@/assets/images/sports/20.svg'
+import sport23 from '@/assets/images/sports/23.svg'
+import sport29 from '@/assets/images/sports/29.svg'
+import sport117 from '@/assets/images/sports/117.svg'
 // import eSports from '../../assets/images/sports/eSports.svg'
 
 import { NavLink } from './NavLink'
@@ -42,15 +44,21 @@ export function SidebarNav() {
         <NavLink src={sport117} href={`/sports/${'mma'}`}>
           MMA
         </NavLink>
+        <NavLink src={sport10} href={`/sports/${'boxe'}`}>
+          Boxe
+        </NavLink>
+        <NavLink src={sport16} href={`/sports/${'american-football'}`}>
+          Futebol Americano
+        </NavLink>
         {/* <NavLink src={eSports} href="#">
           E-Sports
         </NavLink> */}
         <NavLink src={sport23} href={`/sports/${'volleyball'}`}>
           Voleibol
         </NavLink>
-        {/* <NavLink src={eSports} href="#">
+        <NavLink src={sport29} href={`/sports/${'futsal'}`}>
           Futsal
-        </NavLink> */}
+        </NavLink>
       </NavSection>
     </Stack>
   )
