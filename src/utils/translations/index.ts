6d64fe80da18b57d11e608d70c3a7fb49fb84007
lucas vinicius alencar alves <lucasviniciusaalves@gmail.com>
2023-04-27 23:05:34 -0300

    add: Translations

---
+++ src/utils/translations/index.ts
@@ -0,0 +1,5 @@
+import { ptBr } from './pt-br'
+
+export const translate = {
+  'pt-br': ptBr,
+}
---
+++ src/utils/translations/pt-br.ts
@@ -0,0 +1,111 @@
+interface items {
+  logout: string
+  username: string
+  password: string
+  login: string
+  loginError: string
+  loginButton: string
+  register: string
+
+  // Sports
+  soccer: string
+  basketball: string
+  tennis: string
+  volleyball: string
+  handball: string
+  baseball: string
+  americanFootball: string
+  tableTenis: string
+  mma: string
+  boxe: string
+
+  // Menu
+  home: string
+  sports: string
+  myBets: string
+  inPlay: string
+  myAccount: string
+  promotions: string
+  community: string
+  responsible: string
+  rules: string
+  help: string
+  privacy: string
+  terms: string
+  contact: string
+  faq: string
+  aboutUs: string
+
+  // Countries
+  countries: {
+    Brazil: string
+    Argentina: string
+    Chile: string
+    Colombia: string
+    Mexico: string
+    Peru: string
+    Uruguay: string
+    Venezuela: string
+    Bolivia: string
+    Ecuador: string
+    Paraguay: string
+    Guatemala: string
+    Honduras: string
+  }
+}
+
+export const ptBr: items = {
+  logout: 'Sair',
+  username: 'Usuário',
+  password: 'Senha',
+  login: 'Login',
+  loginError: 'Usuário ou senha inválidos',
+  loginButton: 'Entrar',
+  register: 'Registrar-se',
+
+  // Sports
+  soccer: 'Futebol',
+  basketball: 'Basquete',
+  tennis: 'Tênis',
+  volleyball: 'Vôlei',
+  handball: 'Handebol',
+  baseball: 'Baseball',
+  americanFootball: 'Futebol Americano',
+  tableTenis: 'Tênis de Mesa',
+  mma: 'MMA',
+  boxe: 'Boxe',
+
+  // Menu
+  home: 'Início',
+  sports: 'Esportes',
+  myBets: 'Minhas Apostas',
+  inPlay: 'Ao Vivo',
+  myAccount: 'Minha Conta',
+  promotions: 'Promoções',
+  community: 'Comunidade',
+  responsible: 'Jogo Responsável',
+  rules: 'Regras Gerais',
+  help: 'Ajuda',
+  privacy: 'Privacidade',
+  terms: 'Termos de Uso',
+  contact: 'Contato',
+  faq: 'FAQ',
+  aboutUs: 'Sobre Nós',
+
+  // Countries
+  countries: {
+    Brazil: 'Brasil',
+    Argentina: 'Argentina',
+    Chile: 'Chile',
+    Colombia: 'Colômbia',
+    Mexico: 'México',
+    Peru: 'Peru',
+    Uruguay: 'Uruguai',
+    Venezuela: 'Venezuela',
+    Bolivia: 'Bolívia',
+    Ecuador: 'Equador',
+    Paraguay: 'Paraguai',
+    Guatemala: 'Guatemala',
+    Honduras: 'Honduras',
+  },
+}
